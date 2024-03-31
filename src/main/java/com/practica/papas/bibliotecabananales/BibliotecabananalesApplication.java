31b85fd48423812badd59f45f8365aa7be836735
johanp0308 <sebastian-johan1@hotmail.com>
2024-03-31 14:59:19 -0500

    feat: :tada: Init Commit

---
+++ src/main/java/com/practica/papas/bibliotecabananales/BibliotecabananalesApplication.java
@@ -0,0 +1,13 @@
+package com.practica.papas.bibliotecabananales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecabananalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecabananalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica/papas/bibliotecabananales/BibliotecabananalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.papas.bibliotecabananales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecabananalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
